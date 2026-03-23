3de9e58b6050e10dbdd1559e967e768450442749
Saidouchrif <166962901+Saidouchrif@users.noreply.github.com>
2026-03-23 16:09:59 +0100

    version de code avec les fonctions de emprunts

--- Backend/src/Routes/index.js
+++ Backend/src/Routes/index.js
@@ -6,12 +6,15 @@ const authRoutes = require("./authRoutes.js");
 const utilisateurRoutes = require("./utilisateurRoutes.js");
 const categorieRoutes = require("./categorieRoutes.js");
 const livreRoutes = require("./livreRoutes.js");
+const empruntRoutes = require("./empruntRoutes.js");
 
 
 router.use("/auth", authRoutes);
 router.use("/utilisateurs", utilisateurRoutes);
 router.use("/categories", categorieRoutes);
 router.use("/livres", livreRoutes);
+router.use("/emprunts", empruntRoutes);
+
 
 // Centralisation
 module.exports = router;
