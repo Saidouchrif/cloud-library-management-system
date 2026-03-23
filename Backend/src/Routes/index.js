3de7bc1976115a2689e0ca35d8aafac7877a9980
Saidouchrif <166962901+Saidouchrif@users.noreply.github.com>
2026-03-23 15:21:44 +0100

    version de code avec les fonction de categories

--- Backend/src/Routes/index.js
+++ Backend/src/Routes/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 // Import des routes
 const authRoutes = require("./authRoutes.js");
 const utilisateurRoutes = require("./utilisateurRoutes.js");
-
+const categorieRoutes = require("./categorieRoutes.js");
 // Utilisation des routes
 router.use("/utilisateurs", utilisateurRoutes);
-
+router.use("/categories", categorieRoutes);
 router.use("/auth", authRoutes);
 // Centralisation
 module.exports = router;
